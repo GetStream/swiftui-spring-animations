2d7280b39210f7aae6a0f4b63e015302f1cc0cec
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2022-02-10 20:21:08 +0200

    Initial Commit

---
+++ SwiftUISpringAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISpringAnimations
+//
+//  Created by amos.gyamfi@getstream.io on 10.2.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISpringAnimations/SwiftUISpringAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISpringAnimationsApp.swift
+//  SwiftUISpringAnimations
+//
+//  Created by amos.gyamfi@getstream.io on 10.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISpringAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
